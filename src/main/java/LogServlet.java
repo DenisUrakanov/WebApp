1f5228f3baf637311d5ec3d3582ca2fde3a628d5
Denis <papa149@mail.ru>
2018-08-03 10:44:49 +0500

    Log4J using

--- src/main/java/LogServlet.java
+++ src/main/java/LogServlet.java
@@ -1,18 +1,27 @@
+import org.apache.log4j.Logger;
+
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class LogServlet extends HttpServlet {
+
+    private static final Logger log = Logger.getLogger(LogServlet.class);
     @Override
     public void init() throws ServletException {
+        showMessage();
+    }
+
+    private void showMessage(){
         Timer t = new Timer( );
         t.scheduleAtFixedRate(new TimerTask() {
 
             @Override
             public void run() {
-                System.out.println("Hello world");
-
+                log.info("Hello");
+                //System.out.println("Hello world");
             }
         }, 1000,1000);
     }
